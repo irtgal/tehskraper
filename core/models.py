ce56cd7c9e5a5190f8d2e0302d563f944abe62ba
irgal <gal.irt.01@gmail.com>
2020-12-21 14:18:56 +0100

    dodan vue

--- core/models.py
+++ core/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from datetime import datetime
 
 PAGE_CHOICES = (
-	('st', 'https://slo-tech.com/'),
-	('mn', 'https://www.monitor.si/')
+	('st', 'slo-tech'),
+	('mn', 'monitor')
 )
 class Story(models.Model):
 	page =models.CharField(max_length=3, choices=PAGE_CHOICES)
@@ -10,9 +11,17 @@ class Story(models.Model):
 	slug = models.URLField(max_length=200)
 	summary = models.TextField(blank=True, null=True)
 	title = models.CharField(max_length=255, blank=True, null=True)
+	seen = models.BooleanField(default=False)
+	saved = models.BooleanField(default=False)
 
 	def __str__(self):
 		return f'{self.page} - {self.title}'
 
 	class Meta:
 		verbose_name_plural = "Stories"
+
+	def pretty_date(self):
+		return self.date.strftime("%d. %b %Y %H:%M")
+
+	def pretty_page(self):
+		return [full for (short, full) in PAGE_CHOICES if short == self.page][0]
--- core/views.py
+++ core/views.py
@@ -2,11 +2,26 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
+import json
 from django.shortcuts import render
 from .scripts import *
 
 def index(request):
-	get_slotech()
-	get_monitor()
+	#get_slotech()
+	#get_monitor()
 	stories = Story.objects.all().order_by("-date")[:15]
-	return render(request, 'index.html', {'stories': stories})
+	stories_list = []
+	for story in stories:
+		stories_list.append({
+			'id': story.id,
+			'title': story.title,
+			'summary': story.summary,
+			'slug': story.slug,
+			'seen': story.seen,
+			'saved': story.saved,
+			'page': story.pretty_page(),
+			'date': story.pretty_date()
+
+		})
+	print(json.dumps(stories_list))
+	return render(request, 'index.html', {'stories_json': json.dumps(stories_list)})
