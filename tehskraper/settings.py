d0b1ac8cb8cf051ec614e18c4acada3b5d45da60
irgal <gal.irt.01@gmail.com>
2021-03-07 00:17:22 +0100

    dodan procfile

--- tehskraper/settings.py
+++ tehskraper/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -125,3 +126,5 @@ USE_TZ = False
 STATIC_URL = '/static/'
 
 CORS_ORIGIN_ALLOW_ALL = True
+
+django_heroku.settings(locals())
